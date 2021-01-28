0f3cec636b87ef0463726928c511da246a309800
arcy405 <arcy405@gmail.com>
2021-01-28 16:32:39 +0545

    blood donation app creating for mnrypday1

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -1,17 +1,31 @@
 class DonorsController < ApplicationController
   def index
+    @donors=Donor.all
   end
 
   def new
+    @donor=Donor.new
   end
 
   def show
+    @donnar = Donnar.find(params[:id])
+  end
+
+  
+
+  def create
+    @donor = Donor.new(donor_params)
+
+    respond_to do |format|
+      if @donor.save
+        format.html { redirect_to donors_path notice: 'Blood was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
-  end
-
-  def create
   end
 
   def update
@@ -19,4 +33,12 @@ class DonorsController < ApplicationController
 
   def delete
   end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def donor_params
+      params.require(:donor).permit(:name, :address, :gender, :dob, :blood_group, :phone1, :phone2, :status, :verified)
+    end
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_073922) do
+ActiveRecord::Schema.define(version: 2021_01_28_080927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2021_01_28_073922) do
     t.string "verified"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "gender"
   end
 
   create_table "listingrequests", force: :cascade do |t|
