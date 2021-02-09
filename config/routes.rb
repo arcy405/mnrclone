8570feafb5fbc673f25bca78454eedc03dd622a0
Bhuwan-cloud <bhuwanbhatt475@gmail.com>
2021-02-09 13:00:50 +0545

    toiurism in progress

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'homes#index'
   resources :realestates
+  resources :tourists
  
 
   resources :listings do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_153103) do
+ActiveRecord::Schema.define(version: 2021_02_09_061607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -193,6 +193,14 @@ ActiveRecord::Schema.define(version: 2021_02_04_153103) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "listings_count"
+  end
+
+  create_table "tourists", force: :cascade do |t|
+    t.string "place_name"
+    t.string "location"
+    t.string "contact"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
