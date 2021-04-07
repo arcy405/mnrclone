e2ddd8fafec406393a760e4a474c77a57d8b4e00
Naren404 <narendrasunar47@gmail.com>
2021-04-07 15:41:50 +0545

    error pages

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,6 @@ module MnrypDirectoryapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.exceptions_app = self.routes
   end
 end
