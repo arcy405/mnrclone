9dd37961df6fbfb06a6676c96895708034f7dbed
Naren404 <narendrasunar47@gmail.com>
2021-04-16 11:34:48 +0545

    validation final

--- app/models/pet_adoption.rb
+++ app/models/pet_adoption.rb
@@ -1,3 +1,7 @@
 class PetAdoption < ApplicationRecord
+
+	include ActiveModel::Validations
+	
     mount_uploader :image, PetAdoptionUploader
+    validates_presence_of :pet_name, :pet_type, :location, :contact, :image
 end
--- app/models/realestate.rb
+++ app/models/realestate.rb
@@ -1,4 +1,10 @@
 class Realestate < ApplicationRecord
+
+	include ActiveModel::Validations
     extend FriendlyId
+    
   	friendly_id :propertyname, use: :slugged
+
+  	validates_presence_of :seller_name, :seller_phone, :price, :image, :propertyname
+  	
 end
