81e230c2db2a39bfa58080c537ac890763249e35
Naren404 <narendrasunar47@gmail.com>
2021-05-13 19:54:28 +0545

    blood donors improved

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -1,51 +1,58 @@
 class DonorsController < ApplicationController
+
+  before_action :set_donors, only: [:edit, :update, :destroy]
+
   def index
-    @donors=Donor.all.order("created_at DESC")
-    /@donorsApos=Donor.where('blood_group=?',"A+").order("created_at: :DESC")/
-    
+    @donors=Donor.order(:blood_group).where("status=?","Available")
   end
 
   def new
     @donor=Donor.new
   end
 
-  def show
-    @donnar = Donnar.friendly.find(params[:id])
-  end
-
-  
-
   def create
     @donor = Donor.new(donor_params)
 
-    if NewGoogleRecaptcha.human?(
-      params[:new_google_recaptcha_token],
-      "donors",
-      NewGoogleRecaptcha.minimum_score,
-      @post
-      ) && @donor.save
+   respond_to do |format|
+    if  @donor.save
           if user_signed_in?
             current_user.gamification.create!(points:5)
           end
-          format.html { redirect_to donors_path notice: 'Blood was successfully created.' }
+          format.html { redirect_to donors_path, notice: 'Donor was successfully created.' }
       else
         format.html { render :new }
       end
+    end
   end
 
   def edit
+     @donor = Donor.friendly.find(params[:id])
   end
 
   def update
+    respond_to do |format|
+      if @donor.update(donor_params)
+        format.html { redirect_to donors_path, notice: 'Donor was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
-  def delete
+  def destroy
+    @donor.destroy
+    respond_to do |format|
+      format.html { redirect_to donors_path, notice: 'Donor was successfully destroyed.' }
+    end
   end
 
   private
+    def set_donors
+      @donor = Donor.friendly.find(params[:id])
+    end
     # Only allow a list of trusted parameters through.
     def donor_params
-      params.require(:donor).permit(:name, :address, :gender, :blood_group, :phone1, :phone2, :status, :verified,:age)
+      params.require(:donor).permit(:name, :address, :gender, :blood_group,:user_id,:dont_display_phone, :phone1, :phone2, :status, :verified,:age)
     end
 end
 
