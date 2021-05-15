14ad6a0808a891c7e0cd9fc1e36e605de5cb6c10
Naren404 <narendrasunar47@gmail.com>
2021-05-15 13:23:57 +0545

    form errors

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,6 +9,7 @@ class NotesController < ApplicationController
 	end
 
 	def notes_show
+		
 		@chapters = Note.joins(:university).where(universities:{name:params[:uni]}).joins(:department).where(departments:{name:params[:dept]}).joins(:university_semester).where(university_semesters:{name:params[:sem]}).where("subject_name=?",params[:sub])
 	end
 end
