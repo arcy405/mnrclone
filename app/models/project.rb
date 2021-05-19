573b69f3e727d262c63ab183e70fd456dfd14424
Naren404 <narendrasunar47@gmail.com>
2021-05-19 15:03:29 +0545

    friendly id

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ApplicationRecord
+
+	extend FriendlyId
+    
+  	friendly_id :project_name, use: :slugged
+  	
 	mount_uploader :coverimage, ProjectUploader
 	# mount_uploader :coverimage, ProjectbillsUploader
 
@@ -7,5 +12,7 @@ class Project < ApplicationRecord
   	has_many :project_expenses,dependent: :destroy
     accepts_nested_attributes_for :project_images, :project_expenses
     has_many :donations,dependent: :destroy
+
+
 
 end
