ed180cc8966de40752e685cdfeb5d51b86a1c2a7
Naren404 <narendrasunar47@gmail.com>
2021-10-24 12:11:32 +0545

    API for real estate and job vacancy

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -5,6 +5,8 @@ module API
         mount API::V1::Listings
         mount API::V1::Professionals
         mount API::V1::Donors
+        mount API::V1::Realestates
+        mount API::V1::Vacancies
       end
     end
   end
