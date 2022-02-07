cceb537bae2320320362b54723f8035e148bba61
arcy405 <arcy405@gmail.com>
2022-02-07 12:26:24 +0545

    location added to models

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,6 +1,7 @@
 class ModelsController < ApplicationController
 	def index
-		@models=Model.order(:name).includes(:galleries)
+		district = params[:district] || ("Baitadi, Kanchanpur")
+		@models=Model.order(:name).where("district=?", district).includes(:galleries)
 	end
 	def show
 		@model=Model.friendly.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_110310) do
+ActiveRecord::Schema.define(version: 2022_02_07_054110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -353,6 +353,7 @@ ActiveRecord::Schema.define(version: 2022_01_14_110310) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
+    t.string "district"
     t.index ["slug"], name: "index_models_on_slug", unique: true
   end
 
