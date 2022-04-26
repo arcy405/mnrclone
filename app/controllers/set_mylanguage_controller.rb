70499bfbbb663b136bf10183d414aa1f4c31d54d
arcy405 <arcy405@gmail.com>
2022-04-26 15:46:44 +0545

    lang added in blood page

--- app/controllers/set_mylanguage_controller.rb
+++ app/controllers/set_mylanguage_controller.rb
@@ -1,19 +1,37 @@
 class SetMylanguageController < ApplicationController
 	#English
  def en
+    donors = params[:donors] 
   I18n.locale = :en
-  set_session_and_redirect
+  if donors == "donors"
+    set_session_and_redirect2
+  else
+    set_session_and_redirect
+  end
  end
 
  #Nepali
  def np
+    donors = params[:donors] 
   I18n.locale = :np
-  set_session_and_redirect
+  if donors == "donors"
+    set_session_and_redirect2
+  else
+    set_session_and_redirect
+  end
  end
 
  private
+
+ def set_session_and_redirect2
+    session[:locale] = I18n.locale
+    redirect_to donors_path 
+ end
+ 
  def set_session_and_redirect
   session[:locale] = I18n.locale
   redirect_to root_path 
  end
+
+
 end
